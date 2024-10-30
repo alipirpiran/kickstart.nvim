930c2cc6386bfd5362f0a43ce829122fbfc56df3
Ali Pirpiran <pirpiran@amnafzar.ir>
2024-10-30 11:20:50 +0330

    change

--- init.lua
+++ init.lua
@@ -380,6 +380,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+  { 'sindrets/diffview.nvim' },
   { import = 'custom.plugins' },
 }, {
   ui = {
