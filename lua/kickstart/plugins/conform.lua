52c01a49741db460ec0b7389bd9dcfba10aafa47
Ali Pirpiran <pirpiran@amnafzar.ir>
2024-11-03 09:05:10 +0330

    change

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -7,6 +7,10 @@ return {
 
       '<leader>f',
       function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        if vim.b[bufnr].disable_autoformat then
+          return
+        end
         require('conform').format { async = true, lsp_format = 'fallback', formatters = { 'prettier' } }
       end,
       mode = '',
@@ -16,6 +20,11 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      -- Check for buffer-local variable to disable formatting
+      if vim.b[bufnr].disable_autoformat then
+        return
+      end
+
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
@@ -37,8 +46,29 @@ return {
       python = { 'isort', 'black' },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      javascript = { 'prettier', stop_after_first = true },
+      typescript = { 'prettier', stop_after_first = true },
     },
   },
+
+  init = function()
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting globally
+        vim.g.disable_autoformat = true
+      else
+        vim.b.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
 }
