38e4243811253682e5188537fad96ec5fb709291
Ali Pirpiran <pirpiran@amnafzar.ir>
2024-11-05 12:06:50 +0330

    fix

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -3,30 +3,30 @@ return { -- Autocompletion
   event = 'InsertEnter',
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
-    {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-    },
-    'saadparwaiz1/cmp_luasnip',
+    -- {
+    --   'L3MON4D3/LuaSnip',
+    --   build = (function()
+    --     -- Build Step is needed for regex support in snippets.
+    --     -- This step is not supported in many windows environments.
+    --     -- Remove the below condition to re-enable on windows.
+    --     if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+    --       return
+    --     end
+    --     return 'make install_jsregexp'
+    --   end)(),
+    --   dependencies = {
+    --     -- `friendly-snippets` contains a variety of premade snippets.
+    --     --    See the README about individual language/framework/plugin snippets:
+    --     --    https://github.com/rafamadriz/friendly-snippets
+    --     {
+    --       'rafamadriz/friendly-snippets',
+    --       config = function()
+    --         require('luasnip.loaders.from_vscode').lazy_load()
+    --       end,
+    --     },
+    --   },
+    -- },
+    -- 'saadparwaiz1/cmp_luasnip',
 
     -- Adds other completion capabilities.
     --  nvim-cmp does not ship with all sources by default. They are split
@@ -37,16 +37,28 @@ return { -- Autocompletion
   config = function()
     -- See `:help cmp`
     local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
+    -- local luasnip = require 'luasnip'
+    -- luasnip.config.setup {}
 
     cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
+      -- snippet = {
+      --   expand = function(args)
+      --     luasnip.lsp_expand(args.body)
+      --   end,
+      -- },
       completion = { completeopt = 'menu,menuone,noinsert' },
+
+      sorting = {
+        comparators = {
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.kind,
+          -- cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      },
 
       -- For an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`
@@ -107,7 +119,7 @@ return { -- Autocompletion
           group_index = 0,
         },
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+        -- { name = 'luasnip' },
         { name = 'path' },
       },
     }
