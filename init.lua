9392bb69162835a00c1a6c770aa36e47afb9f2c3
Ali Pirpiran <pirpiran@amnafzar.ir>
2024-11-19 14:13:42 +0330

    change

--- init.lua
+++ init.lua
@@ -327,24 +327,6 @@ require('lazy').setup({
   -- Autoformat
   require 'kickstart.plugins.conform',
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -366,7 +348,7 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { 'sindrets/diffview.nvim' },
-  { 'xiyaowong/transparent.nvim' },
+  -- { 'xiyaowong/transparent.nvim' },
   { import = 'custom.plugins' },
 }, {
   ui = {
